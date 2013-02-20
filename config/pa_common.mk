e9cacd207692ada2da81862e7342e59b1ddc4833
Eugene San (eugenesan) <eugenesan@gmail.com>
2013-02-20 20:20:45 +0200

    supersu 1.10
    Change-Id: Ia5afe66bc8ec4a8112a5895e4888df4bd12b6b1a

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -15,7 +15,7 @@ PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/common/apk/Term.apk:system/app/Term.apk \
     vendor/pa/prebuilt/common/lib/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
     vendor/pa/prebuilt/common/apk/GooManager.apk:system/app/GooManager.apk \
-    vendor/pa/prebuilt/common/apk/SuperSU.apk:system/app/SuperSU.apk \
+    vendor/pa/prebuilt/common/apk/Superuser.apk:system/app/Superuser.apk \
     vendor/pa/prebuilt/common/xbin/su:system/xbin/su \
     vendor/pa/prebuilt/common/xbin/su:system/bib/.ext/.su
 
